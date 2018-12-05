cbb7a19b059bbc9ef375666e959cb107575127f7
Baldur Haraldsson <baldurhar18@ru.is>
2018-12-05 16:39:20 +0000

    back takki

--- repo/CustomerRepo.py
+++ repo/CustomerRepo.py
@@ -1,4 +1,5 @@
-from models.Customer import Customer 
+from models.Customer import Customer
+
 
 class CustomerOptions:
 
@@ -6,31 +7,31 @@ class CustomerOptions:
         self.__customer = []
 
     def add_customer(self, customer):
-        #first add to file then to private list
+        # first add to file then to private list
         try:
-            with open('./data/customers.txtt', 'a+') as customer_file: #a+ = creates file if it doesnt exist
+            # a+ = creates file if it doesnt exist
+            with open('./data/customers.txtt', 'a+') as customer_file:
                 name = customer.get_name()
                 socialnumber = customer.socialnumber()
                 phonenumber = customer.get_phonenumber()
                 email = customer.get_email()
-                customer_file.write('{},{},{},{}'.format(self.__name, self.__socialnumber, self.__phonenumber, self.__email))
+                customer_file.write('{},{},{},{}'.format(
+                    self.__name, self.__socialnumber, self.__phonenumber, self.__email))
         except:
-            #adda þessu í skránna??? 1:18:20 i fyrirlestri 2
+            # adda þessu í skránna??? 1:18:20 i fyrirlestri 2
             pass
 
     def get_customer(self):
-        if self.__customer == []: #first time this function is used
-            with open('./data/customers.txt','r') as customer_file:
+        if self.__customer == []:  # first time this function is used
+            with open('./data/customers.txt', 'r') as customer_file:
                 for line in customer_file.readlines():
                     name, socialnumber, phonenumber, email = line.split(",")
-                    new_costumer = Customer(name, socialnumber, phonenumber, email)
+                    new_costumer = Customer(
+                        name, socialnumber, phonenumber, email)
                     self.__customer.append(new_costumer)
                 return self.__customer
         else:
             return self.__customer
 
-
-    def delete_customer(self):
-        with open('./data/customers.txtt', 'a+') as customer_file:
-
- 
+    # def delete_customer(self):
+        # with open('./data/customers.txtt', 'a+') as customer_file:
