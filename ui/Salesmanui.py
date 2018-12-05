3b0acb27d20f7cab79dc1e4b48d11af601bac108
Birta <birtab18@ru.is>
2018-12-05 11:56:48 +0000

    breyta f samræmi i UI

--- ui/Salesmanui.py
+++ ui/Salesmanui.py
@@ -14,33 +14,29 @@ class Frontpage:
             action = input('Choose option: ').lower()
 
             if action =='1':
-                print('Press 1 for Sign Customer up')
+                print('Press 1 to Sign Up New Customer ')
                 print('Press 2 to Delete Customer')
-                print('Press 3 for Look up Cusoumer')
-                print('Press 4 for Change information about customer')
+                print('Press 3 to Look Up Customer')
+                print('Press 4 to Change Information About A Customer')
                 action2= input('Choose option: ').lower()
 
                 if action2 == '1':
                     name = input('Enter a name: ')
                     socialnumber = input('Enter a number: ')
                     phonenumber = input('Enter a phonenumber: ')
-                    email = input('Enter a email: ')
+                    email = input('Enter email: ')
                     new_costumer = Customer(name, socialnumber, phonenumber,email)
                     self.__customer_main.add_customer(new_costumer)
                 
      
             elif action == '2':
-                print('Press 1 for Set a car free')
-                print('Press 2 for set a car in rental')
-                print('Press 3 for Put in an order')
-                print('Press 4 for Delete order')
-                print('Press 5 for Look up a order')
-                print('Press 6 for Change a order')
-                print('Press 7 for showing cars status')
-                print('Press 8 for showing how much each car cost')
-
-
-
-                #TEST
+                print('Press 1 to Set a car free')
+                print('Press 2 to Set a car in rental')
+                print('Press 3 to Put in an order')
+                print('Press 4 to Delete Order')
+                print('Press 5 to Look Up A Order')
+                print('Press 6 to Change A Order')
+                print('Press 7 to Show Cars Status')
+                print('Press 8 to Show Price List')
 
 
