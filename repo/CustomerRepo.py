21b67b944d21fd5404380de980accdba4b7550ba
Bergrós <bergros18@ru.is>
2018-12-05 16:13:20 +0000

    delete_cusomer

--- repo/CustomerRepo.py
+++ repo/CustomerRepo.py
@@ -5,28 +5,27 @@ class CustomerOptions:
     def __init__(self):
         self.__customer = []
 
-    def add_customer(self, customer):
-        #first add to file then to private list
-        try:
-            with open('./data/customers.csv', 'a+') as customer_file: #a+ = creates file if it doesnt exist
-                name = customer.get_name()
-                socialnumber = customer.socialnumber()
-                phonenumber = customer.get_phonenumber()
-                email = customer.get_email()
-                customer_file.write('{},{},{},{}'.format(self.__name, self.__socialnumber, self.__phonenumber, self.__email))
-        except:
-            #adda þessu í skránna??? 1:18:20 i fyrirlestri 2
-            pass
+    def add_Customer(self, customer):
+        with open('./data/customers.txt', 'a+') as customer_file: #a+ stendur fyrir ad skra verdur buin til ef thetta er ekki til
+            name = customer.get_length()
+            socialnumber = customer.get_socialnumber()
+            phonenumber = customer.get_phonenumber()
+            email = customer.get_email()
+            customer_file.write('{},{},{},{}'.format(name, socialnumber, phonenumber, email))
 
-    def get_customer(self):
-        if self.__customer == []: #first time this function is used
-            with open('./data/customers.csv','r') as customer_file:
-                for line in customer_file.readlines():
-                    name, socialnumber, phonenumber, email = line.split(",")
-                    new_costumer = Customer(name, socialnumber, phonenumber, email)
-                    self.__customer.append(new_costumer)
-                return self.__customer
-        else:
-            return self.__customer
+    def get_videos(self):
+        customer =[]
+        with open('./data/customers.txt','r') as customer_file:
+            for line in customer_file.readlines():
+                name, socialnumber, phonenumber, email = line.split(",")
+                new_costumer = Customer(name, socialnumber, phonenumber, email)
+                customer.append(new_costumer)
+            return customer
+
+    
+    def delete_Customer:
+        with open('./data/customers.txt','r') as customer_file:
+
+
 
  
