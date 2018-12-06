b8ee3737b568730c91ccbcce3e4124299c795236
Birta <birtab18@ru.is>
2018-12-06 13:48:29 +0000

    category

--- ui/CarUi.py
+++ ui/CarUi.py
@@ -24,7 +24,7 @@ class Car_Page:
             print('Press 7 to Show Cars Availability')  # True or False
             print('Press 8 to Show Price List')
             print('Press 9 to Add A New Car To The Car Rental') 
-            print('Press 10 to Change The Price List')
+            print('Press 10 to Change The Price List')  #ekki lagmarkskrafa
             print('Press q to Quit')
         
         # def sign_Up():
@@ -71,12 +71,33 @@ class Car_Page:
             elif action_Car == '9':
                 print("-"*15)
                 print("New customer:")
-                licence_Plate = input('Enter The Licence Plate: ')
+                licence_Plate = input('Enter The Licence Plate: ').upper()
+                category = input('Enter The Category (M = Mini Car, P = Passenger Car, J = Jeep): ').lower()
+                #þurfum að gera eh svona lykkju
+                # category = ''
+                # while category != 'm' or 'p' or 'j': 
+                #     category = input('Enter The Category (M = Mini Car, P = Passenger Car, J = Jeep): ').lower()
+                #     if category == 'm':
+                #         category = 'Mini Car'
+                #     elif category == 'p':
+                #         category = 'Passenger Car'
+                #     elif category == 'j':
+                #         category = 'Jeep'
+                #     else: 
+                #         print("Invalid input, try again!")
                 manufacturer = input('Enter The Manufacturer: ')
                 the_Type = input('Enter The Type: ')
-                transmission = input('Stick Shift Or Manual?: ')
+                transmission = input('Enter The Transmission (S = Stick Shift, M = Manual): ').lower()
+                #þurfum að gera eh svona lykkju
+                # while transmission != 's' or 'm':
+                #     if transmission == 's':
+                #         transmission == 'Stick Shift'
+                #     elif transmission == 'm':
+                #         transmission == 'Manual'
+                #     else:
+                #         print('Invalid input, try again!')
                 price = input('Enter Price: ')
-                new_Car = Car(licence_Plate, manufacturer, the_Type, transmission, price)
+                new_Car = Car(licence_Plate, category, manufacturer, the_Type, transmission, price)
                 self.__CarService.add_car(new_Car)
             elif action_Car == '10':
                 pass
