94da24d6a87681af3a45bc769647fa6fd4de1eb8
Helgi <helgist18@ru.is>
2018-12-10 14:31:40 +0000

    breyting

--- services/CustomerService.py
+++ services/CustomerService.py
@@ -9,10 +9,10 @@ class CustomerService:
         if self.is_valid_customer(customer):  # eru innputin sett in rett
             self.__customer_Repo.add_customer(customer)  # ATH hvort repo eda main
 
-    def is_valid_customer(self, customer):
-        # herna rékkum vi hvort þetta video se ekki orugglega hægt af vinna med.
-        # ef valid þa addum vid þvi i gagnageymsluna.
-        return True
+    # def is_valid_customer(self, customer):
+    #     # herna rékkum vi hvort þetta video se ekki orugglega hægt af vinna med.
+    #     # ef valid þa addum vid þvi i gagnageymsluna.
+    #     return True
 
     def look_up_customer(self, SSN):
         return self.__customer_Repo.look_up_customer(SSN)  # ATH hvort repo eda main
